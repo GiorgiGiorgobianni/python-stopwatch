ec59b9c53dac829f06253a63cf2d97b5768d32c8
Giorgi Giorgobiani <129969647+GiorgiGiorgobianni@users.noreply.github.com>
2023-07-20 21:39:32 +0400

    Add files via upload

---
+++ stopwatch.py
@@ -0,0 +1,33 @@
+
+import os
+import time
+
+seconds = 0
+minutes = 0
+hours = 0
+
+
+while(True):
+    print('\t\t\t\t  %d : %d : %d ' % (seconds, minutes, hours))
+    time.sleep(1)
+    seconds+=1
+
+    if seconds == 60:
+        (seconds == 0)
+        minutes+=1
+
+    if minutes == 60:
+        (minutes == 0)
+        hours+=60
+    os.system('cls')
+
+
+
+
+
+
+
+
+
+
+
